29d24a983aef8d716270eb792f55c12bee01e564
cortana <cortana@bb-101>
2024-07-26 17:17:36 +0100

    Add work

--- games/cortana/game.py
+++ games/cortana/game.py
@@ -1,26 +1,50 @@
+# @name Name of my Amazing Example Game
+# @author Jane & John Smith
 import pgzrun
+import sys
+import random
 
 WIDTH = 800
 HEIGHT = 600
 
-ship = Actor('playership1_blue')
+ship = Actor('playership1_red')
 ship.x = 370
 ship.y = 550
 
+gem = Actor('gemgreen')
+gem.x = random.randint(20, 780)
+gem.y = 0
+
+score = 0
+game_over = False
+
 def update():
+  global score, game_over
+
+  if keyboard.escape:
+    exit()
+    sys.exit()
   if keyboard.left:
     ship.x = ship.x - 5
   if keyboard.right:
     ship.x = ship.x + 5
 
-def draw():
-  screen.fill((80,0,70))
-  ship.draw()
+  gem.y = gem.y + 4 + score / 5
+  if gem.y > 600:
+    game_over = True
+  if gem.colliderect(ship):
+    gem.x = random.randint(20, 780)
+    gem.y = 0
+    score = score + 1
 
-pgzrun.go() # Must be last line
 def draw():
   screen.fill((80,0,70))
-  gem.draw()
-  ship.draw()
+  if game_over:
+    screen.draw.text('Game Over', centerx=WIDTH/2, top=200, color=(255,255,255), fontsize=150)
+    screen.draw.text('Final Score: ' + str(score), centerx=WIDTH/2, top=320, color=(255,255,255), fontsize=50)
+  else:
+    gem.draw()
+    ship.draw()
+    screen.draw.text('Score: ' + str(score), (15,10), color=(255,255,255), fontsize=30)
 
 pgzrun.go() # Must be last line
