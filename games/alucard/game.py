830aaca8f33c14c89a0208aedb4026f654e1ce66
jill_valentine <jill_valentine@bb-105>
2024-07-28 12:31:13 +0100

    Revert "Add game"
    This reverts commit ff3c6c51561ffb95f695adfced7e6d99acccd790.

--- games/alucard/game.py
+++ games/alucard/game.py
@@ -1,8 +1,12 @@
 # @name Name of my Amazing Example Game
 # @author Jane & John Smith
 import pgzrun
-import random
 import sys
+import random
+from pgzero.builtins import Actor, keyboard
+import pgzero.screen
+screen : pgzero.screen.Screen
+
 
 WIDTH = 800
 HEIGHT = 600
@@ -11,17 +15,9 @@ ship = Actor('playership1_blue')
 ship.x = 370
 ship.y = 550
 
-ship2 = Actor('playership1_red')
-ship2.x = 270
-ship2.y = 550
-
 gem = Actor('gemgreen')
 gem.x = random.randint(20, 780)
 gem.y = 0
-
-gem2 = Actor('gemred')
-gem2.x = random.randint(20, 780)
-gem2.y = 0
 
 score = 0
 game_over = False
@@ -36,10 +32,6 @@ def update():
     ship.x = ship.x - 5
   if keyboard.right:
     ship.x = ship.x + 5
-  if keyboard.a:
-    ship2.x = ship2.x - 5
-  if keyboard.d:
-    ship2.x = ship2.x + 5
 
   gem.y = gem.y + 4 + score / 5
   if gem.y > 600:
@@ -48,14 +40,6 @@ def update():
     gem.x = random.randint(20, 780)
     gem.y = 0
     score = score + 1
-  if gem.colliderect(ship2):
-    gem.x = random.randint(20, 780)
-    gem.y = 0
-    score = score + 1
-    if gem2.colliderect(ship2):
-     gem2.x = random.randint(20, 780)
-    gem2.y = 0
-    score = score + 2
 
 def draw():
   screen.fill((80,0,70))
@@ -64,9 +48,7 @@ def draw():
     screen.draw.text('Final Score: ' + str(score), centerx=WIDTH/2, top=320, color=(255,255,255), fontsize=50)
   else:
     gem.draw()
-    gem2.draw()
     ship.draw()
-    ship2.draw()
     screen.draw.text('Score: ' + str(score), (15,10), color=(255,255,255), fontsize=30)
 
 pgzrun.go() # Must be last line
