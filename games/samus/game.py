7fdf715ada8c62337b6fee81e7d4f1ae7c15ff9f
samus <samus@bb-103>
2024-07-28 12:41:09 +0100

    Add work

---
+++ games/samus/game.py
@@ -0,0 +1,58 @@
+# @name Name of my Amazing Example Game
+# @author Jane & John Smith
+import pgzrun
+import random
+import sys
+
+WIDTH = 800
+HEIGHT = 600
+
+ship = Actor('playership1_blue')
+ship.x = 370
+ship.y = 550
+
+gem = Actor('gemgreen')
+gem.x = random.randint(20, 780)
+gem.y = 0
+
+asteroid = Actor('gemred')
+score = 0
+game_over = False
+
+
+def update():
+  global score, game_over
+  if keyboard.left:
+    ship.x = ship.x - 5
+  if keyboard.right:
+    ship.x = ship.x + 5
+  if keyboard.up:
+    ship.y = ship.y - 5
+  if keyboard.down:
+    ship.y = ship.y + 5
+
+  gem.y = gem.y + 4
+  if gem.y > 600:
+    game_over = True
+  if gem.colliderect(ship):
+    gem.x = random.randint(20, 780)
+    gem.y = 0
+    score = score + 1
+    
+    asteroid.y = asteroid.y + 4
+    game_over = True
+  if asteroid.colliderect(ship):
+    game_over = True
+    
+
+def draw():
+  screen.fill((80,0,70))
+  if game_over:
+    screen.draw.text('Game Over', centerx=WIDTH/2, top=200, color=(255,255,255), fontsize=150)
+    screen.draw.text('Final Score: ' + str(score), centerx=WIDTH/2, top=320, color=(255,255,255), fontsize=50)
+  else:
+    gem.draw()
+    ship.draw()
+    screen.draw.text('Score: ' + str(score), (15,10), color=(255,255,255), fontsize=30)
+
+pgzrun.go() # Must be last line
